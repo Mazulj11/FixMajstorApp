f2f6a0cd1fce25bd5eb2ba428124651223a8a1ec
Mateo <mateo.zulj11@gmail.com>
2024-01-02 22:58:50 +0100

    implemented login and register layouts and activity

--- app/src/main/java/com/example/advertisementapp/MainActivity.java
+++ app/src/main/java/com/example/advertisementapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.advertisementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button openRegisterBtn = findViewById(R.id.openRegisterBtn);
+
+        openRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        Button openLoginBtn = findViewById(R.id.openLoginBtn);
+
+        openLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
     }
 }
