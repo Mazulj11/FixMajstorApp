5fe2cb7f4cab22bb266e645aac48c3b4ff08e752
Mateo <mateo.zulj11@gmail.com>
2024-01-16 11:44:54 +0100

    implement user jobs layout

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,6 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.android.application") version "8.2.1" apply false
 }
