9147d432e7db4f7c38ebdc9eb4340d0a85f0ae31
Mateo <mateo.zulj11@gmail.com>
2024-01-23 22:01:02 +0100

    implement add new job

---
+++ app/src/main/java/com/example/fixmajstorapp/AddJobActivity.java
@@ -0,0 +1,173 @@
+package com.example.fixmajstorapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import com.example.fixmajstorapp.model.Job;
+
+public class AddJobActivity extends AppCompatActivity {
+
+    Button selectImageBtn;
+    Button saveBtn;
+    ImageView imagePreview;
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance("https://advertisementapp-e50f9-default-rtdb.firebaseio.com/");
+
+    Uri filePath;
+    String jobImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_job);
+        this.selectImageBtn = findViewById(R.id.selectImageBtn);
+        this.saveBtn = findViewById(R.id.saveBtn);
+        this.imagePreview = findViewById(R.id.imagePreview);
+
+        this.storage = FirebaseStorage.getInstance();
+        this.storageReference = this.storage.getReference();
+
+        selectImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
+            }
+        });
+
+        EditText professionEditTxt = findViewById(R.id.professionEditText);
+        EditText nameEditTxt = findViewById(R.id.fullnameEditText);
+        EditText locationEditTxt = findViewById(R.id.locationEditText);
+        EditText addressEditTxt = findViewById(R.id.addressEditText);
+
+//        Button movieSaveBtn = findViewById(R.id.saveMovieBtn);
+
+        DatabaseReference jobsReference = mDatabase.getReference("/jobs");
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String profession = professionEditTxt.getText().toString();
+                String name = nameEditTxt.getText().toString();
+                String location = locationEditTxt.getText().toString();
+                String address = addressEditTxt.getText().toString();
+                Double rating = 0.0;
+                Job j = new Job( name, location, address, profession, jobImage, rating);
+                jobsReference.push().setValue(j);
+                Intent i = new Intent(AddJobActivity.this, JobActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton openHomeBtn = findViewById(R.id.homeBtn);
+        openHomeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(AddJobActivity.this, HomeActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        ImageButton openJobsBtn = findViewById(R.id.openJobsBtn);
+        openJobsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(AddJobActivity.this, JobActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 22 &&
+                resultCode == RESULT_OK &&
+                data != null &&
+                data.getData() != null) {
+            this.filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imagePreview.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void selectImage () {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(i, "Odaberite sliku posla"), 22
+        );
+    }
+
+    private void uploadImage () {
+        if (filePath != null) {
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Učitavam sliku");
+            progressDialog.show();
+            StorageReference ref = storageReference.child("images/"
+                    + UUID.randomUUID().toString()
+            );
+            ref.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progressDialog.hide();
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Slika je učitana na server",
+                            Toast.LENGTH_LONG).show();
+                    ref.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            jobImage = task.getResult().toString();
+                        }
+                    });
+                }
+            });
+
+
+
+        }
+    }
+}
