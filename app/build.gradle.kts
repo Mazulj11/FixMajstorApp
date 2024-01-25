a26e75ab020299466433c3d4c8bb93c5243d1b22
Mateo <mateo.zulj11@gmail.com>
2024-01-25 11:11:29 +0100

    rollback2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation ("com.firebaseui:firebase-ui-database:8.0.2")
--- app/src/main/java/com/example/fixmajstorapp/HomeActivity.java
+++ app/src/main/java/com/example/fixmajstorapp/HomeActivity.java
@@ -1,14 +1,18 @@
 package com.example.fixmajstorapp;
 
+
 import android.annotation.SuppressLint;
+
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -16,6 +20,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
 
         ImageButton openJobsBtn = findViewById(R.id.openJobsBtn);
         openJobsBtn.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +30,7 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(registerIntent);
             }
         });
+
 
 
     }
